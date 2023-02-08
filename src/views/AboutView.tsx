aef11d8f5c433935a4b1d392f641d15bcc4045e7
HawkWang <wph2021@foxmail.com>
2023-02-08 15:33:42 +0800

    feat: 集成Piana

--- src/views/AboutView.tsx
+++ src/views/AboutView.tsx
@@ -1,11 +1,17 @@
 import { defineComponent } from 'vue';
+import { useCounterStore } from '@/store';
+import { storeToRefs } from 'pinia';
 
 export default defineComponent({
   name: 'AboutPage',
   setup() {
+    const counterStore = useCounterStore();
+    const { count } = storeToRefs(counterStore);
+    const { increment } = counterStore;
     return () => (
       <>
         <div>This is About Page</div>
+        <button onClick={increment}>count from store {count.value}</button>
       </>
     );
   },
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,15 +1,21 @@
 import HelloTsx from '@/components/HelloTsx';
 import HelloWorld from '@/components/HelloWorld.vue';
 import { defineComponent } from 'vue';
+import { useCounterStore } from '@/store';
+import { storeToRefs } from 'pinia';
 
 export default defineComponent({
   name: 'HomeView',
   setup() {
+    const counterStore = useCounterStore();
+    const { count } = storeToRefs(counterStore);
+    const { increment } = counterStore;
     return () => (
       <>
         <div>This is Home Page</div>
         <HelloTsx />
         <HelloWorld msg="Vite + Vue" />
+        <button onClick={increment}>count from store {count.value}</button>
       </>
     );
   },
